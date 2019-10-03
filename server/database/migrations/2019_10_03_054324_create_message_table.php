b0df78d0d53697aa4604eece0ebcab979bd8a5a3
masami <kei11nishikori16@gmail.com>
2019-10-03 14:45:05 +0900

    Table追加

---
+++ server/database/migrations/2019_10_03_054324_create_message_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('talk_id');
+            $table->unsignedBigInteger('user_id');
+            $table->multiLineString('message');
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message');
+    }
+}
